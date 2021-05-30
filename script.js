06b94102e8df176371b860d12f9931976b30a575
arttLynx <higher1234566@gmail.com>
2021-05-30 12:46:06 +0300

    final Changes

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 //AOS.init();
-if (document.documentElement.clientWidth <= 767) {
-    var href = document.location.href;
-    var href2 = href.split('.', 2);
-    alert("It is " + href2[0] + href2[1]);
-    window.location.replace((href2[0] + 'Mobile.html'));
+window.onload = function () {
+    var style = document.getElementById("switchStyle");
+    var switchBtn = document.getElementById("switchBtn");
+
+    switchBtn.onclick = function () {
+        if (style.getAttribute('href') == 'style.css') {
+            style.href = "darkTheme.css";
+            switchBtn.innerHTML = "LightMode";
+            return 0;
+        }
+        else {
+            style.href = "style.css";
+            switchBtn.innerHTML = "DarkMode";
+            return 0;
+        }
+    }
+
+    if (document.documentElement.clientWidth <= 767) {
+        var href = document.location.href;
+        var href2 = href.split('.', 2);
+        alert("It is " + href2[0] + href2[1]);
+        window.location.replace((href2[0] + 'Mobile.html'));
+    }
 }
